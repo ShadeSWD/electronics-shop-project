f9f0b02461261e3e8de935a6b6ba67870a8580aa
Shade_SWD <seawolfy60@gmail.com>
2023-07-07 17:39:55 +0300

    hw4 item

--- src/item.py
+++ src/item.py
@@ -16,35 +16,48 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         self.all.append(self)
 
-    def __repr__(self):
-        return f"Item('{self.__name}', {self.price}, {self.quantity})"
+    def __repr__(self) -> str:
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
 
-    def __str__(self):
-        return self.__name
+    def __str__(self) -> str:
+        return self.name
+
+    def __add__(self, other) -> int:
+        """
+        Складывает количество товаров
+
+        :return суммарное количество
+        """
+        if not isinstance(other, Item):
+            raise ValueError('Складывать можно только объекты Item и дочерние от них.')
+        return self.quantity + other.quantity
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.__name
 
     @name.setter
-    def name(self, name: str):
+    def name(self, name: str) -> None:
         self.__name = name[:10]
 
     @classmethod
     def instantiate_from_csv(cls):
+        """
+                Создание экземпляров класса item из csv.
+        """
         with open('../src/items.csv') as csv_file:
             reader = csv.DictReader(csv_file)
             for row in reader:
                 cls(name=row['name'], price=row['price'], quantity=row['quantity'])
 
     @staticmethod
-    def string_to_number(str_number: str):
+    def string_to_number(str_number: str) -> int:
         """
         Takes a number in str and converts it to integer
 
