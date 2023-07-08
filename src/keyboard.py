6afb2f427c84cd19bf9449f5f4c0a09952e29255
Shade_SWD <seawolfy60@gmail.com>
2023-07-08 13:32:42 +0300

    fix keyboard

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,37 +1,41 @@
 from src.item import Item
 
 
-class Keyboard(Item):
+class LanguageMixin:
     """
-    Класс для представления товара "клавиатура" в магазине.
+    Миксин для поддержки изменения языка (раскладки клавиатуры).
+
+        __language - язык расскладки клавиавтуры, по умолчанию 'EN'
     """
-    languages = ['EN', 'RU']
 
-    def __init__(self, name: str, price: float, quantity: int, language: str = 'EN') -> None:
-        """
-        Создание экземпляра класса phone.
-
-        :param language: Язык раскладки.
-        """
-        super().__init__(name=name, price=price, quantity=quantity)
-        self.__language = language
-
-    def __repr__(self) -> str:
-        return f"Keyboard('{self.name}', {self.price}, {self.quantity}, {self.language})"
+    def __init__(self):
+        self.__language = 'EN'
 
     @property
     def language(self) -> str:
+        """
+        Возвращает текущий язык (раскладку клавиатуры).
+
+        :return: Текущий язык.
+        """
         return self.__language
 
     def change_lang(self):
-        """
-        Изменяет язык клавиатуры
-
-        :return self
-        """
-        if self.__language == self.languages[0]:
-            self.__language = self.languages[1]
-        else:
-            self.__language = self.languages[0]
-
+        self.__language = "RU" if self.language == "EN" else "EN"
         return self
+
+
+class Keyboard(Item, LanguageMixin):
+    """
+    Класс для представления товара "клавиатура" в магазине.
+    """
+
+    def __init__(self, name: str, price: float, quantity: int,) -> None:
+        """
+        Создание экземпляра класса keyboard.
+        """
+        super().__init__(name=name, price=price, quantity=quantity)
+        LanguageMixin.__init__(self)
+
+    def __repr__(self) -> str:
+        return f"Keyboard('{self.name}', {self.price}, {self.quantity}, {self.language})"
